bed46e737be72c4a1682b1139f1bcdd579104241
athanar <102956385+athanar@users.noreply.github.com>
2024-04-07 13:13:23 +0200

    ajout openAI

--- src/openAI.ts
+++ src/openAI.ts
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 
 export const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: "Your api key",
   dangerouslyAllowBrowser: true,
 });
