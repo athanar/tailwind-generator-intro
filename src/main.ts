df7d564ddb85beba4ea709c8bd6127490ab6098c
athanar <102956385+athanar@users.noreply.github.com>
2024-04-07 16:14:38 +0200

    use stream openAPI

--- src/main.ts
+++ src/main.ts
@@ -10,23 +10,59 @@ form.addEventListener('submit', async (event) => {
   const formData = new FormData(form);
   const prompt = formData.get('prompt') as string;
   
-  const chatCompletion = await openai.chat.completions.create({
+  const response = await openai.chat.completions.create({
     messages: [
       { role: "system", content: "Tu crées des sites web avec tailWind, ta tâche est de générer du code HTML avec TailWind en fonction du prompt de l'utilisateur"+
        ", Tu renvoie uniquement du HTML sans texte avant ou après. Tu renvoie du HTML valide. Tu ne renvoie jamais de balises markdown" },
       { role: "user", content: prompt }],
     model: "gpt-3.5-turbo",
+    stream: true,
   });
 
-  const code = chatCompletion.choices[0].message.content;
+  let code = '';
+  const onNewChunck = createTimeUpdateIframe();
+
+  for await (const message of response) {
+    const isDone = message.choices[0].finish_reason === 'stop';
+    const token = message.choices[0].delta?.content;
+    code += token;
+    if(isDone){
+
+    }
+    onNewChunck(code);
+    //process.stdout.write(chunk.choices[0]?.delta?.content || "");
+  }
+
+  //const code = chatCompletion.choices[0].message.content;
 
   //console.log('code : '+code);
 
-  if(!code) {
-    alert('aucun code généré');
-    return;
-  }
-
-  iframe.srcdoc = "<html><head><script src='https://cdn.tailwindcss.com'></script></head><body>" + String(code) + "</body></html>";
+  return;
+ 
 
 });
+
+const createTimeUpdateIframe = () => {
+  let date = new Date();
+  let timeout : any = null;
+
+  return (code: string) => {
+    if(new Date().getTime() - date.getTime() > 1000){
+      updateIframe(code);
+      date = new Date();
+    }
+
+    if(timeout){
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      updateIframe(code);
+    },1000);
+
+}
+}
+
+const updateIframe = (code: string) => {
+  iframe.srcdoc = '<!DOCTYPE HTML><html><head><script src="https://cdn.tailwindcss.com"></script></head><body>'+code+'</body></html>';
+}
